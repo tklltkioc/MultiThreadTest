0d06802df2063fa9d91ef9c9669c95d241e6c888
tklltkioc <wusiyuong@gmail.com>
2019-04-16 21:53:29 +0800

    等待/通知机制

--- WitNotify.java
+++ WitNotify.java
@@ -1,16 +1,26 @@
 package threadtest;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class WitNotify {
     static boolean flag=true;
     static Object lock=new Object();
 
     public static void main(String[] args) {
-        Thread waitThread=new Thread();
+        Thread waitThread=new Thread(new Wait(),"WaitThread");
+        waitThread.start();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread notifyThread=new Thread(new Notify(),"notifyThread");
+        notifyThread.start();
     }
     static class Wait implements Runnable{
+        @Override
         public void run(){
             synchronized (lock){
                 while (flag){
@@ -30,7 +40,16 @@ public class WitNotify {
     static class Notify implements Runnable{
         public void run(){
             synchronized (lock){
-
+                System.out.println(Thread.currentThread()+"hold lock."+
+                        new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                lock.notifyAll();
+                flag=false;
+                SleepUntils.second(5);
+            }
+            synchronized (lock){
+                System.out.println(Thread.currentThread()+"hold again lock."+
+                        new  SimpleDateFormat("HH:mm:ss").format(new Date()));
+                SleepUntils.second(5);
             }
         }
     }
