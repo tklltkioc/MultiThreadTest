96b14e8b131c478d0fc78dfa371348d91dd53893
tklltkioc <wusiyuong@gmail.com>
2019-09-19 11:37:29 +0800

    修改注释

--- atomictest/AtomicReferenceDemo2.java
+++ atomictest/AtomicReferenceDemo2.java
@@ -14,9 +14,9 @@ public class AtomicReferenceDemo2 {
         User user = new User ("ss", 18);
         atomicUserRef.set (user);
         System.out.println (atomicUserRef.get ().toString ());
-        User updateUser = new User ("sa", 25);
+        User updateUser = new User ("sd5a", 25);
         atomicUserRef.compareAndSet (user, updateUser);
-        //执行结果:User{name='sa', age=25}
+        // 执行结果:User{name='sd5a', age=25}
         System.out.println (atomicUserRef.get ().toString ());
     }
 
